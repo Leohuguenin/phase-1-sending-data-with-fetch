7c8ba094d438ab18756ef01b88dca402c42ceb42
Leohuguenin <leonardohuguenin@live.com>
2025-04-20 17:01:26 +1000

    fetch

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+    function submitData (name, email) {
+    const userData = {
+        name: name,
+        email: email,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(object) {
+            const body = document.querySelector("body");
+            const p = document.createElement("p");
+            p.textContent = object.id;
+            body.appendChild(p);
+          })
+          .catch(function(error) {
+            const body = document.querySelector("body");
+            const p = document.createElement("p");
+            p.textContent = error.message;
+            body.appendChild(p);
+          });
+      }
+      
+      submitData("Leo", "leo@example.com");
